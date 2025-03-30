1c964ac8f02f2321610fd7a92519de21bf8e0e18
group8g <116568735+group8g@users.noreply.github.com>
2025-03-30 17:26:53 +0300

    add sub category food list

---
+++ lib/view/screen/sandwich_screen.dart
@@ -0,0 +1,221 @@
+import 'package:capstone/model/product_item_model.dart';
+import 'package:capstone/view/widget/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class SandwichScreen extends StatelessWidget {
+  const SandwichScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<ProductItemModel> sandwichList = [
+      ProductItemModel(price: 20.00, imageUrl: 'assets/images/pizza3.png', description: AppLocalizations.of(context)!.product_ing, name: AppLocalizations.of(context)!.pepperoni_pizza),
+      ProductItemModel(price: 20.00, imageUrl: 'assets/images/pizza3.png', description:AppLocalizations.of(context)!.product_ing, name: AppLocalizations.of(context)!.pepperoni_pizza),
+      ProductItemModel(price: 20.00, imageUrl: 'assets/images/pizza3.png', description: AppLocalizations.of(context)!.product_ing, name: AppLocalizations.of(context)!.pepperoni_pizza),
+      ProductItemModel(price: 20.00, imageUrl: 'assets/images/pizza3.png', description: AppLocalizations.of(context)!.product_ing, name: AppLocalizations.of(context)!.pepperoni_pizza),
+    ];
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body:  ListView.builder(
+        itemCount:2,
+        padding: EdgeInsets.only(top: 20,left: 21,right: 21,bottom: 20),
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              children: [
+                Stack(
+                  children: [
+                    // name & description & price
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.23,
+                      margin: EdgeInsets.only(top: 30, bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 219, 244, 209),
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 60),
+                          // product name
+                          TextWidget(
+                            text: sandwichList[index].name,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            letterSpacing: -0.03,
+                            fontColor: Colors.black,
+                          ),
+                          SizedBox(height: 5),
+                          // ingredients
+                          TextWidget(
+                            text: sandwichList[index].description,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 10,
+                            fontFamily: 'Inter',
+                            letterSpacing: 0,
+                            fontColor: Color.fromARGB(255,150, 154, 176),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          TextWidget(
+                            text: '& ${sandwichList[index].price.toString()}' ,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            letterSpacing: -0.03,
+                            fontColor: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // image
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(60),
+                        border: Border.all(color: Colors.grey, width: 10),
+                      ),
+                      margin: EdgeInsets.only(left: 32, right: 32),
+                      child: Image.asset(
+                        sandwichList[index].imageUrl,
+                        width: 70,
+                        height: 70,
+                      ),
+                    ),
+
+                    // add to favorite
+                    Container(
+                      width: 35,
+                      height: 35,
+                      margin: EdgeInsets.only(left: 130, top: 10),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 219, 244, 209),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Icon(Icons.favorite_border, size: 13),
+                    ),
+
+                    // order Now
+                    Container(
+                      width: 95,
+                      height: 27,
+
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 205),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 37, 174, 75),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Center(child: TextWidget(text: 'Order Now', fontWeight: FontWeight.w400, fontSize: 10, fontFamily: 'Inter', letterSpacing: 0,fontColor: Colors.white,)),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 20),
+                Stack(
+                  children: [
+                    // name & description & price
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: MediaQuery.of(context).size.height * 0.23,
+                      margin: EdgeInsets.only(top: 30, bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Color.fromARGB(255, 219, 244, 209),
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 60),
+                          // product name
+                          TextWidget(
+                            text: sandwichList[index].name,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            letterSpacing: -0.03,
+                            fontColor: Colors.black,
+                          ),
+                          SizedBox(height: 5),
+                          // ingredients
+                          TextWidget(
+                            text: sandwichList[index].description,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 10,
+                            fontFamily: 'Inter',
+                            letterSpacing: 0,
+                            fontColor: Color.fromARGB(255,150, 154, 176),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          TextWidget(
+                            text: '& ${sandwichList[index].price.toString()}' ,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            fontFamily: 'Inter',
+                            letterSpacing: -0.03,
+                            fontColor: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // image
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(60),
+                        border: Border.all(color: Colors.grey, width: 10),
+                      ),
+                      margin: EdgeInsets.only(left: 32, right: 32),
+                      child: Image.asset(
+                        sandwichList[index].imageUrl,
+                        width: 70,
+                        height: 70,
+                      ),
+                    ),
+
+                    // add to favorite
+                    Container(
+                      width: 35,
+                      height: 35,
+                      margin: EdgeInsets.only(left: 130, top: 10),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 219, 244, 209),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Icon(Icons.favorite_border, size: 13),
+                    ),
+
+                    // order Now
+                    Container(
+                      width: 95,
+                      height: 27,
+
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 205),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 37, 174, 75),
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Center(child: TextWidget(text: 'Order Now', fontWeight: FontWeight.w400, fontSize: 10, fontFamily: 'Inter', letterSpacing: 0,fontColor: Colors.white,)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/view/widget/bottom_notification_sheet_widget.dart
@@ -0,0 +1,79 @@
+// import 'package:capstone/view/screen/all_notification.dart';
+// import 'package:capstone/view/screen/read_notification.dart';
+// import 'package:capstone/view/screen/unread_notification.dart';
+import 'package:capstone/view/widget/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class BottomNotificationSheetWidget extends StatelessWidget {
+  const BottomNotificationSheetWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(
+            MediaQuery.of(context).size.height * 0.1,
+          ),
+          child: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            title: TextWidget(
+              text: AppLocalizations.of(context)!.notification,
+              fontWeight: FontWeight.w600,
+              fontSize: 24,
+              fontFamily: 'Inter',
+              letterSpacing: 0,
+            ),
+            bottom: TabBar(
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorColor: Color.fromARGB(255, 37, 174, 75),
+              indicatorWeight: 2,
+              dividerColor: Colors.white,
+              labelColor: Color.fromARGB(255, 37, 174, 75),
+
+              tabs: [
+                // All
+                TextWidget(
+                  text: AppLocalizations.of(context)!.all,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                  fontFamily: 'Inter',
+                  letterSpacing: 0,
+                  textAlign: TextAlign.center,
+                ),
+                // Unread
+                TextWidget(
+                  text: AppLocalizations.of(context)!.unread,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                  fontFamily: 'Inter',
+                  letterSpacing: 0,
+                  textAlign: TextAlign.center,
+                ),
+                // Read
+                TextWidget(
+                  text: AppLocalizations.of(context)!.read,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                  fontFamily: 'Inter',
+                  letterSpacing: 0,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(children: [
+
+
+          Text('All'),
+          Text('Unread'),
+          Text('read'),
+        ]),
+      ),
+    );
+  }
+}
