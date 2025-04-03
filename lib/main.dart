98f1f5a3839212078d4a883f0e9455791e1b4514
group8g <116568735+group8g@users.noreply.github.com>
2025-04-03 18:37:14 +0300

    edit start screen in main

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyAppState extends State<MyApp> {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
         debugShowCheckedModeBanner: false,
-        home: MainScreen(),
+        home: SplashScreen(),
       ),
     );
   }
