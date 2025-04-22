6e58cb3f6c5df852c3b0cacaa7d84aa732b0f490
group8g <116568735+group8g@users.noreply.github.com>
2025-04-22 23:01:43 +0300

    edit font size &  color in theme contoller

--- lib/controller/theme_controller.dart
+++ lib/controller/theme_controller.dart
@@ -18,28 +18,28 @@ class ThemeController extends ChangeNotifier {
       surface: Colors.grey[900]!,
       onSurface: Colors.white,
     ),
-    textTheme: const TextTheme(
-      bodyLarge: TextStyle(color: Colors.white, fontSize: 22),
-      bodyMedium: TextStyle(color: Colors.white, fontSize: 28),
-      bodySmall: TextStyle(color: Colors.white, fontSize: 15),
-      labelLarge: TextStyle(color: Colors.white, fontSize: 25),
-    ),
+    // textTheme: const TextTheme(
+    //   bodyLarge: TextStyle(color: Colors.white, fontSize: 22),
+    //   bodyMedium: TextStyle(color: Colors.white, fontSize: 28),
+    //   bodySmall: TextStyle(color: Colors.white, fontSize: 15),
+    //   labelLarge: TextStyle(color: Colors.white, fontSize: 25),
+    // ),
   );
 
   final ThemeData light = ThemeData(
     brightness: Brightness.light,
     colorScheme: ColorScheme.light(
-      primary: Colors.blue,
+      primary: Colors.black,
       secondary: Colors.amber,
       surface: Colors.white,
       onSurface: Colors.black,
     ),
-    textTheme: const TextTheme(
-      bodyLarge: TextStyle(color: Colors.black, fontSize: 22),
-      bodyMedium: TextStyle(color: Colors.black, fontSize: 28),
-      bodySmall: TextStyle(color: Colors.black, fontSize: 15),
-      labelLarge: TextStyle(color: Colors.black, fontSize: 25),
-    ),
+    // textTheme: const TextTheme(
+    //   bodyLarge: TextStyle(color: Colors.black, fontSize: 22),
+    //   bodyMedium: TextStyle(color: Colors.black, fontSize: 28),
+    //   bodySmall: TextStyle(color: Colors.black, fontSize: 15),
+    //   labelLarge: TextStyle(color: Colors.black, fontSize: 25),
+    // ),
   );
 
   Future<void> _initializeTheme() async {
